006691ffd812760f0c9ea00ce796381104df8a84
Guilherme <guilhermemagno09@gmail.com>
2023-02-11 00:04:06 -0300

    working on localStorage

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,51 +1,67 @@
-import React, { useState } from "react"
-import Botao from "../Botao"
-import {Tarefa} from "../../types/tarefa"
-import {v4 as uuidv4} from "uuid"
-import "./style.scss"
+import React, { useEffect, useState } from "react";
+import Botao from "../Botao";
+import { Tarefa } from "../../types/tarefa";
+import { v4 as uuidv4 } from "uuid";
+import "./style.scss";
 
-const Formulario = ({setTarefas}:{setTarefas: React.Dispatch<React.SetStateAction<Tarefa[]>>})=> {
-    const [tarefa, setTarefa] = useState("")
-    const [tempo, setTempo] = useState("00:00")
+interface ISetTarefas {
+  setTarefas: React.Dispatch<React.SetStateAction<Tarefa[]>>;
+  tarefas: Tarefa[]
+}
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        setTarefas((antigaTarefa) => [...antigaTarefa, {tarefa: tarefa, tempo: tempo,
-        selecionado: false, completado: false, id: uuidv4() }])
-        setTarefa("")
-        setTempo("")
-    }
-    
-    return (
+const Formulario = ({ setTarefas }: ISetTarefas) => {
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState("00:00:00");
+
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setTarefas((antigaTarefa) => [
+      ...antigaTarefa,
+      {
+        tarefa: tarefa,
+        tempo: tempo,
+        selecionado: false,
+        completado: false,
+        id: uuidv4(),
+      },
+    ]);
+    setTarefa("");
+    setTempo("");
+  };
+
+  return (
     <form className="novaTarefa" onSubmit={handleSubmit}>
-        <div className="inputContainer">
+      <div className="inputContainer">
         <label htmlFor="tarefa">Adicione um novo estudo</label>
-        <input 
-        type="text" 
-        id="tarefa" 
-        name="tarefa" 
-        value={tarefa}
-        onChange={event => setTarefa(event.target.value)}
-        placeholder="Oque voce quer estudar" 
-        required/>
-        
-        </div>
-        <div className="inputContainer">
-            <label htmlFor="tempo">Tempo</label>
-            <input 
-            type="time" 
-            id="tempo" 
-            name="tempo" 
-            step='1'
-            value={tempo}
-            onChange={event => setTempo(event.target.value)}
-            min="00:00:00" 
-            max='10:00:00' 
-            required/>
-            </div>
-            <Botao type="submit">Adicionar</Botao>
-            </form>
-        )
-    }
+        <input
+          type="text"
+          id="tarefa"
+          name="tarefa"
+          value={tarefa}
+          onChange={(event) => setTarefa(event.target.value)}
+          placeholder="Oque voce vai estudar"
+          required
+        />
+      </div>
+      <div className="inputContainer">
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          id="tempo"
+          name="tempo"
+          step="1"
+          value={tempo}
+          onChange={(event) => setTempo(event.target.value)}
+          min="00:00:00"
+          max="05:00:00"
+          required
+        />
+      </div>
+      <Botao type="submit">Adicionar</Botao>
+    </form>
+  );
+};
 
-export default Formulario
+export default Formulario;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,45 +1,65 @@
-import React, { useState } from 'react';
-import Cronometro from '../components/Cronometro';
-import Formulario from '../components/Formulario';
-import Lista from '../components/Lista';
-import  "../styles/style.scss";
-import { Tarefa } from '../types/tarefa';
+import  { useEffect, useState } from "react";
+import Cronometro from "../components/Cronometro";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import "../styles/style.scss";
+import { Tarefa } from "../types/tarefa";
 
 function App() {
-  const [tarefas, setTarefas] = useState<Tarefa[]>([])
-  const [selecionado, setSelecionado] = useState<Tarefa>()
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<Tarefa>();
 
-  function selecionaTarefa(tarefaSelecionada:Tarefa){
-    setSelecionado(tarefaSelecionada)
-    setTarefas(oldOnes => oldOnes.map((tarefa) => ({
-      ...tarefa,
-      selecionado: tarefaSelecionada.id === tarefa.id ? true : false
-    })))
+  useEffect(() => {
+    if(tarefas.length != 0) {
+      localStorage.setItem('key', JSON.stringify(tarefas));
+    }    
+    if(localStorage.hasOwnProperty('key')) {
+      let data:any = localStorage.getItem('key')
+      const parsedData = JSON.parse(data)
+      setTarefas(parsedData)
+    }
+  },[tarefas])
+
+
+
+  function selecionaTarefa(tarefaSelecionada: Tarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((oldOnes) =>
+      oldOnes.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefaSelecionada.id === tarefa.id ? true : false,
+      }))
+    );
   }
 
-
-  function finalizarTarefa(){
-    if(selecionado) {
-      setSelecionado(undefined)
-      setTarefas(oldOnes => oldOnes.map((tarefa) => {
-        if(tarefa.id === selecionado.id){
-          return {
-            ...tarefa,
-            selecionado: false,
-            completado: true
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined);
+      setTarefas((oldOnes) =>
+        oldOnes.map((tarefa) => {
+          if (tarefa.id === selecionado.id) {
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true,
+            };
           }
-        }
 
-        return tarefa
-      }))
-    } 
+          return tarefa;
+        })
+      );
+    }
   }
 
   return (
-    <div className='AppStyle'>
-      <Formulario setTarefas={setTarefas}/>
-      <Lista selecionaTarefa={selecionaTarefa} tarefas={tarefas}/>
-      <Cronometro selecionado={selecionado} finalizarTarefa={finalizarTarefa}/>
+    <div className="AppStyle">
+      <div>
+        <Formulario tarefas={tarefas} setTarefas={setTarefas} />
+        <Cronometro selecionado={selecionado}finalizarTarefa={finalizarTarefa}/>
+      </div>
+      <div className="lista">
+        <Lista selecionaTarefa={selecionaTarefa} tarefas={tarefas} />
+      </div>
     </div>
   );
 }
